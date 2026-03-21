3a7a95bbf646152fb9eeb3e83d621a6cd679be1b
Likhi17 <likhithlikhi17@gmail.com>
2026-03-22 01:46:22 +0530

    Playwright

---
+++ code/04_basics2/test_mousehoverclick.py
@@ -0,0 +1,14 @@
+import time
+
+import pytest
+from playwright.sync_api import sync_playwright, expect
+
+
+def test_keyboard_click():
+    browser = sync_playwright().start().chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+    page.goto("https://the-internet.herokuapp.com/")
+    page.locator("//a[normalize-space()='Broken Images']").dblclick()
+    #.hover(),.click(modifiers=["shift","control","alt"]
+    time.sleep(5)
